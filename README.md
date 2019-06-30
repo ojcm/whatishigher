b8575fd7cffac148d5c13ef78e09b72d83f52c22
ojcm <ojcmuir@gmail.com>
2019-06-30 11:58:46 +0100

    Remove answers from html with server side logic

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Web-based game using Python Flask
 # To Do
 With more time I would improve this app in the following ways (in no particular order):
 - Get a constant image size to improve layout.
-- Obfuscate the answers in the HTML. I started this (see `markCorrectHash` in `flash_app.py`) and thought it would be an opportunity to use a hash function but didn’t have time to finish it.
 - Implement a running total of correct answers. And then a leaderboard.
 - Better layout on small devices (mobile).
 - Improve button styling (bootstrap/semantic ui?).
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-from random import randint
 import hashlib
+import random
 
 app = Flask(__name__)
 
@@ -11,7 +11,6 @@ class Higher():
         self.short_name = short_name
         self.height = height
         self.image = image
-        self.correct = ''
 
     # Is this object taller than the comparison object
     def isHigher(self, comparison):
@@ -21,16 +20,16 @@ class Higher():
 class Game():
 
     # The objects that will be compared in the game
-    elements = [Higher('Eiffel Tower', 'ET', 324, '/static/images/et.jpeg'),
-                Higher('Burj Khalifa', 'BK', 828, '/static/images/bk.jpeg'),
-                Higher('One World Trade Center', 'WT', 541, '/static/images/wt.jpeg'),
-                Higher('Taipei 101', 'TP', 508, '/static/images/tp.jpeg'),
-                Higher('Petronas Towers', 'PT', 452, '/static/images/pt.jpeg'),
-                Higher('Empire State Building', 'ES', 381, '/static/images/es.jpeg'),
-                Higher('The Shard', 'SH', 310, '/static/images/sh.jpeg'),
-                Higher('Eureka Tower', 'EU', 297, '/static/images/eu.jpeg'),
-                Higher('Big Ben', 'BB', 96, '/static/images/bb.jpeg'),
-                Higher('Statue of Liberty', 'SL', 93, '/static/images/sl.jpeg')]
+    elements = {'ET': Higher('Eiffel Tower', 'ET', 324, '/static/images/et.jpeg'),
+                'BK': Higher('Burj Khalifa', 'BK', 828, '/static/images/bk.jpeg'),
+                'WT': Higher('One World Trade Center', 'WT', 541, '/static/images/wt.jpeg'),
+                'TP': Higher('Taipei 101', 'TP', 508, '/static/images/tp.jpeg'),
+                'PT': Higher('Petronas Towers', 'PT', 452, '/static/images/pt.jpeg'),
+                'ES': Higher('Empire State Building', 'ES', 381, '/static/images/es.jpeg'),
+                'SH': Higher('The Shard', 'SH', 310, '/static/images/sh.jpeg'),
+                'EU': Higher('Eureka Tower', 'EU', 297, '/static/images/eu.jpeg'),
+                'BB': Higher('Big Ben', 'BB', 96, '/static/images/bb.jpeg'),
+                'SL': Higher('Statue of Liberty', 'SL', 93, '/static/images/sl.jpeg')}
 
     # Compare the two options, mark the taller one as correct and vice-versa
     def markCorrect(self, option1, option2):
@@ -41,31 +40,19 @@ class Game():
             option1.correct = 'incorrect'
             option2.correct = 'correct'
 
-    # UNUSED.  Method to create a hash indicating whether the item is correct or not.
-    def getCorrectHash(self, shortname, correct):
-        return hashlib.sha256(shortname.encode('utf-8') + b'correct').hexdigest() if correct else hashlib.sha256(shortname.encode('utf-8') + b'incorrect').hexdigest()
-
-    # UNUSED.  Method to add hashes to object to indicate their correctness.
-    def markCorrectHash(self, option1, option2):
-        option1.correct = self.getCorrectHash(option1.short_name, option1.isHigher(option2))
-        option2.correct = self.getCorrectHash(option2.short_name, option2.isHigher(option1))
-
     # Pick two random objects from elements
     def getChoices(self):
-        element_count = len(self.elements)
 
         # If there is only one element then we get stuck in an infinite
         # while-loop.  Assert to avoid that.
-        assert(element_count > 1)
+        assert(len(self.elements) > 1)
 
         # Get the two options
-        first_option = randint(0, element_count-1)
-        second_option = randint(0, element_count-1)
+        first_option = random.choice(list(self.elements.keys()))
+        second_option = random.choice(list(self.elements.keys()))
         while second_option == first_option:
-            second_option = randint(0, element_count-1)
+            second_option = random.choice(list(self.elements.keys()))
 
-        # Mark one option correct and one incorrect
-        self.markCorrect(self.elements[first_option], self.elements[second_option])
         return [self.elements[first_option], self.elements[second_option]]
 
     # Run the game
@@ -95,15 +82,19 @@ def higher_game():
     return Game().runGame()
 
 # Recurring route. Display result from previous round and new question.
-@app.route('/answer', methods=['POST'])
+@app.route('/answer', methods=['POST', 'GET'])
 def answer():
-    # Check if user pressed the correct button (correct) or correct image (correct.x)
-    if 'correct' in request.form or 'correct.x' in request.form:
+    game = Game()
+    answer = request.args.get('answer', None)
+    other = request.args.get('other', None)
+
+    if game.elements[answer].isHigher(game.elements[other]):
         response = Response('Correct! :)', True)
     else:
         response = Response('Incorrect. :(', False)
 
     # start a new game
-    return Game().runGame(response)
+    return game.runGame(response)
     # start a new game and display some debugging information
     #return Game().runGame(response, debug=request.form)
+
